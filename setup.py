f84c5fe891a24b3e084699bc1e60536817a93556
Jake Schrass <39309741+jaschrass@users.noreply.github.com>
2019-06-24 12:37:20 -0500

    Setup file
    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='supersonic_morphing',
+      version='0.1',
+      description='Makes pictures from supersonic morphing',
+      url='https://github.com/maestrolab/supersonic_morphing',
+      author='Jacob Schrass',
+      author_email='jlostinco@tamu.edu',
+      license='none',
+      packages=['supersonic_morphing'],
+      zip_safe=False)
